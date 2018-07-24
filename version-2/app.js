c9b6ed2b7f3080a256fb180d248fe2b0f898cf60
Thomas Clukies <Thomas@Thomass-MacBook-Pro.local>
2018-07-24 14:03:21 -0600

    moving to own repo

---
+++ version-2/app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3002;
+const csvToJson = require("convert-csv-to-json");
+const instructors = csvToJson
+    .fieldDelimiter(",")
+    .getJsonFromCsv("./instructors.csv");
+
+app.use(cors());
+
+function findById(data, id) {
+    for (let i = 0; i < data.length; i++) {
+        let holder = data[i].ID.toString();
+        if (holder === id) {
+            return data[i];
+        }
+    }
+}
+
+app.get("/", (req, res) => {
+    res.json({ data: instructors });
+});
+
+app.get("/:id", (req, res) => {
+    var record = findById(instructors, req.params.id);
+    if (!record) {
+        res.status = 404;
+        res.json({
+            error: {
+                message: "No record found!"
+            }
+        });
+    } else {
+        res.json({ data: record });
+    }
+});
+
+app.listen(port, () => {
+    console.log("listening on port", port);
+});
